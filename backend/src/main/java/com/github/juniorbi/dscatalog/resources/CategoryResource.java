1d4a05e02e46b2d12237279b790b46a9d19967ab
Antonio Junior <antonio.silvajunior.as@gmail.com>
2022-09-14 10:39:44 -0300

    dto

--- backend/src/main/java/com/github/juniorbi/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/github/juniorbi/dscatalog/resources/CategoryResource.java
@@ -1,5 +1,6 @@
 package com.github.juniorbi.dscatalog.resources;
 
+import com.github.juniorbi.dscatalog.dto.CategoryDTO;
 import com.github.juniorbi.dscatalog.entities.Category;
 import com.github.juniorbi.dscatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,8 +18,8 @@ public class CategoryResource {
     @Autowired
     private CategoryService service;
     @GetMapping
-    public ResponseEntity<List<Category>> findAll() {
-        List<Category> list = service.findAll();
+    public ResponseEntity<List<CategoryDTO>> findAll() {
+        List<CategoryDTO> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 }
--- backend/src/main/java/com/github/juniorbi/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/github/juniorbi/dscatalog/services/CategoryService.java
@@ -1,12 +1,15 @@
 package com.github.juniorbi.dscatalog.services;
 
+import com.github.juniorbi.dscatalog.dto.CategoryDTO;
 import com.github.juniorbi.dscatalog.entities.Category;
 import com.github.juniorbi.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
@@ -14,7 +17,10 @@ public class CategoryService {
     @Autowired
     private CategoryRepository repository;
     @Transactional(readOnly = true)
-    public List<Category> findAll() {
-        return repository.findAll();
+    public List<CategoryDTO> findAll() {
+        List<Category> list = repository.findAll();
+
+        return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+
     }
 }
